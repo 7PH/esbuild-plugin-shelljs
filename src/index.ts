8936312bcf3b9cf7bdd11209835d7cbc16dfd99e
Benjamin Raymond <31401273+7PH@users.noreply.github.com>
2024-09-19 14:56:18 +0200

    Fix lifecycle hook usage

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,37 @@
-import { Plugin, PluginBuild } from 'esbuild';
+import { Plugin } from 'esbuild';
 import shell from 'shelljs';
 
 const name = 'esbuild-plugin-shelljs';
 
-
 export const shellJsPlugin: Plugin = {
     name: 'shelljs',
 
-    setup(build: PluginBuild) {
-        build.onStart(() => {
-            shell.exec(`npx patch-package --patch-dir node_modules/${name}/patches`);
-        });
+    setup(build) {
+        function getCdCommand(): string | null {
+            if (build.initialOptions.absWorkingDir) {
+                return `cd ${build.initialOptions.absWorkingDir}`;
+            }
+            return null;
+        }
 
-        build.onEnd(() => {
-            shell.exec(`npx patch-package --reverse --patch-dir node_modules/${name}/patches`);
+        function getPatchCommand(reverse?: boolean): string {
+            let command = `npx patch-package --patch-dir node_modules/${name}/patches`;
+            if (reverse) {
+                command += ' --reverse';
+            }
+            return command;
+        }
+
+        function getCommand(reverse?: boolean): string {
+            return [getCdCommand(), getPatchCommand(reverse)].filter(Boolean).join(' && ');
+        }
+
+        // Patch on plugin setup
+        shell.exec(getCommand());
+
+        // Unpatch on build end
+        build.onDispose(() => {
+            shell.exec(getCommand(true));
         });
     },
 };
